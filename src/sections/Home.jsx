a55970ab9da9406e60645782a6d4059818129abb
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2026-05-02 03:01:47 -0600

    feat: new color palette

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,8 +1,10 @@
+import AsciiPortrait from "../components/AsciiPortrait"
 
 export default function Home() {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center">
         <h1 className="text-4xl font-bold">Home</h1>
+        <AsciiPortrait />
     </section>
   )
 }
